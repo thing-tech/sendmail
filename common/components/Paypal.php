6b6a92e672bab0401d1dca50119565cb8c97750d
Vinh Huynh Tu <vinhhuynhtu@Vinhs-MacBook-Pro.local>
2017-11-07 15:28:29 +0700

    paypay

---
+++ common/components/Paypal.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace common\components;
+
+use Yii;
+use yii\base\ErrorException;
+use yii\helpers\ArrayHelper;
+use yii\base\Component;
+use PayPal\Api\Address;
+use PayPal\Api\CreditCard;
+use PayPal\Api\Amount;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\Transaction;
+use PayPal\Api\FundingInstrument;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\Details;
+use PayPal\Rest\ApiContext;
+
+class Paypal extends Component {
+
+    //region Mode (production/development)
+    const MODE_SANDBOX = 'sandbox';
+    const MODE_LIVE = 'live';
+    //endregion
+    //region Log levels
+    /*
+     * Logging level can be one of FINE, INFO, WARN or ERROR.
+     * Logging is most verbose in the 'FINE' level and decreases as you proceed towards ERROR.
+     */
+    const LOG_LEVEL_FINE = 'FINE';
+    const LOG_LEVEL_INFO = 'INFO';
+    const LOG_LEVEL_WARN = 'WARN';
+    const LOG_LEVEL_ERROR = 'ERROR';
+
+    //endregion
+    //region API settings
+    public $clientId;
+    public $clientSecret;
+    public $isProduction = false;
+    public $currency = 'USD';
+    public $config = [
+        'http.ConnectionTimeOut' => 30,
+        'http.Retry' => 1,
+        'mode' => self::MODE_SANDBOX,
+        'log.LogEnabled' => YII_DEBUG ? 1 : 0,
+        'log.FileName' => '@runtime/logs/paypal.log',
+        'log.LogLevel' => self::LOG_LEVEL_FINE,
+    ];
+
+    /** @var ApiContext */
+    private $_apiContext = null;
+
+    /**
+     * @setConfig 
+     * _apiContext in init() method
+     */
+    public function init() {
+        $this->setConfig();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    private function setConfig() {
+        // ### Api context
+        // Use an ApiContext object to authenticate
+        // API calls. The clientId and clientSecret for the
+        // OAuthTokenCredential class can be retrieved from
+        // developer.paypal.com
+        $this->_apiContext = new ApiContext(
+                new OAuthTokenCredential(
+                $this->clientId, $this->clientSecret
+                )
+        );
+        // #### SDK configuration
+        // Comment this line out and uncomment the PP_CONFIG_PATH
+        // 'define' block if you want to use static file
+        // based configuration
+        $this->_apiContext->setConfig(ArrayHelper::merge(
+                        [
+                    'mode' => self::MODE_SANDBOX, // development (sandbox) or production (live) mode
+                    'http.ConnectionTimeOut' => 30,
+                    'http.Retry' => 1,
+                    'log.LogEnabled' => YII_DEBUG ? 1 : 0,
+                    'log.FileName' => Yii::getAlias('@runtime/logs/paypal.log'),
+                    'log.LogLevel' => self::LOG_LEVEL_FINE,
+                    'validation.level' => 'log',
+                    'cache.enabled' => 'true'
+                        ], $this->config)
+        );
+        // Set file name of the log if present
+        if (isset($this->config['log.FileName']) && isset($this->config['log.LogEnabled']) && ((bool) $this->config['log.LogEnabled'] == true)
+        ) {
+            $logFileName = \Yii::getAlias($this->config['log.FileName']);
+            if ($logFileName) {
+                if (!file_exists($logFileName)) {
+                    if (!touch($logFileName)) {
+                        throw new ErrorException('Can\'t create paypal.log file at: ' . $logFileName);
+                    }
+                }
+            }
+            $this->config['log.FileName'] = $logFileName;
+        }
+        return $this->_apiContext;
+    }
+
+    //Demo
+    public function payDemo() {
+        $addr = new Address();
+        $addr->setLine1('52 N Main ST');
+        $addr->setCity('Johnstown');
+        $addr->setCountryCode('US');
+        $addr->setPostalCode('43210');
+        $addr->setState('OH');
+        $card = new CreditCard();
+        $card->setNumber('4417119669820331');
+        $card->setType('visa');
+        $card->setExpireMonth('11');
+        $card->setExpireYear('2018');
+        $card->setCvv2('874');
+        $card->setFirstName('Joe');
+        $card->setLastName('Shopper');
+        $card->setBillingAddress($addr);
+        $fi = new FundingInstrument();
+        $fi->setCreditCard($card);
+        $payer = new Payer();
+        $payer->setPaymentMethod('credit_card');
+        $payer->setFundingInstruments(array($fi));
+        $amountDetails = new Details();
+        $amountDetails->setSubtotal('15.99');
+        $amountDetails->setTax('0.03');
+        $amountDetails->setShipping('0.03');
+        $amount = new Amount();
+        $amount->setCurrency('USD');
+        $amount->setTotal('7.47');
+        $amount->setDetails($amountDetails);
+        $transaction = new Transaction();
+        $transaction->setAmount($amount);
+        $transaction->setDescription('This is the payment transaction description.');
+        $payment = new Payment();
+        $payment->setIntent('sale');
+        $payment->setPayer($payer);
+        $payment->setTransactions(array($transaction));
+        return $payment->create($this->_apiContext);
+    }
+
+}

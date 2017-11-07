5aec69a3c009abe2a88645ebb8273aa57bff8f4d
Vinh Huynh Tu <vinhhuynhtu@Vinhs-MacBook-Pro.local>
2017-11-07 15:02:40 +0700

    paypal

---
+++ common/queue/DownloadJob.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class DownloadJob extends BaseObject implements \yii\queue\JobInterface
+{
+    public $url;
+    public $file;
+    
+    public function execute($queue)
+    {
+        file_put_contents($this->file, file_get_contents($this->url));
+    }
+}
